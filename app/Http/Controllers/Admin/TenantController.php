29f6ca748507c13c5a87fb4ede8e2a1918165984
alamin <alamincsetpi@gmail.com>
2026-03-01 22:37:49 +0600

    feat: update

--- app/Http/Controllers/Admin/TenantController.php
+++ app/Http/Controllers/Admin/TenantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Actions\Tenant\CreateTenantAction;
 use Illuminate\Http\Request;
+// bangla
 class TenantController
 {
    public function store(Request $request, CreateTenantAction $action)
